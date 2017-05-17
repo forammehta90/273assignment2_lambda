e9124becd9d573f67debcc0d161e78b8c9fc1107
foram mehta <forammehta90@gmail.com>
2017-05-17 16:17:43 -0700

    Lambda

---
+++ pizzashopmenuput/service.py
@@ -0,0 +1,16 @@
+import boto3
+
+
+def handler(event, context):
+    # Your code goes here!
+    client = boto3.client("dynamodb")
+    
+    try:
+        client.update_item(TableName="pizzashopmenu", Key=event, UpdateExpression="SET #sel = list_append(#sel, :val1)",ExpressionAttributeNames={"#sel": "selection"}, ExpressionAttributeValues={":val1":{"L":[{"S":"Vegetable"}]}})
+        item = client.get_item(TableName="pizzashopmenu", Key=event)
+        response = {}
+        response ["menu_id"] = item["Item"]["menu_id"]["S"]
+        response ["selection"] = [select["S"] for select in item["Item"]["selection"]["L"]]
+    except Exception,e:
+        return 400, e
+    return response, 200, "OK"
